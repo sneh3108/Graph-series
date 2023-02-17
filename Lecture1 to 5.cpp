f00ff2272e46167f96aa5f58e053c47d12e9b29e
SNEH RANJAN <100509471+sneh3108@users.noreply.github.com>
2023-02-17 17:13:56 +0530

    Update Lecture1 to 5.cpp

--- Lecture1 to 5.cpp
+++ Lecture1 to 5.cpp
@@ -1,6 +1,6 @@
 // storing a graph
 
-
+// Method1 Space complexity high O(N*N)
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -19,7 +19,28 @@ int main(){
   adj[v][u]=1;
  }
  return 0;
- 
-
-    
 }
+
+
+
+
+// Method2 Space Complexity good  O(2E)  E = no of Edges;
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ int n,m;
+ cin>>n>>m;
+ vector<int> adj[n+1];
+ for(int i=0;i<m;i++){
+  int u,v;
+  cin>>u>>v;
+  adj[u].push_back(v);
+  adj[v].push_back(u);
+ }
+ return 0;
+}
+
+
+
