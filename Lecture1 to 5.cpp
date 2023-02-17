b7a97b6aec898108f3dc017234a1e9614b592aec
SNEH RANJAN <100509471+sneh3108@users.noreply.github.com>
2023-02-17 17:17:21 +0530

    Update Lecture1 to 5.cpp

--- Lecture1 to 5.cpp
+++ Lecture1 to 5.cpp
@@ -44,3 +44,26 @@ int main(){
 
 
 
+// for directed graph
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ int n,m;
+ cin>>n>>m;
+ vector<int> adj[n+1];
+
+ for(int i=0;i<m;i++){
+  int u,v;
+  cin>>u>>v;
+  // u ---> v  means edge between u and v which is directed from u to v
+  adj[u].push_back(v);
+  
+ }
+ return 0;
+}
+
+
+
